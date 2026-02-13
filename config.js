b897d64d8dcd3c677eb91f7b15935b0458991845
OFCsoftwareDevops <olaoluwa.ogunseye@gmail.com>
2026-02-13 22:30:29 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,10 @@
 const appLinks = {
   playStore: {
     url: "https://play.google.com/store/apps/details?id=com.ofcsoftwareDevops.rccgsundayschoolmanual",  // ← update if needed
+    badge: null
   },
   appStore: {
     url: "https://apps.apple.com/app/idYOUR_APP_ID",
+    badge: null
   }
 };

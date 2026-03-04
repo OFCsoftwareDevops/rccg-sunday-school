324e264da079b014146a8ce11b86e613292c58e2
OFCsoftwareDevops <olaoluwa.ogunseye@gmail.com>
2026-03-04 20:31:38 +0100

    Update app links and badges in config.js

--- config.js
+++ config.js
@@ -2,10 +2,10 @@
 const appLinks = {
   playStore: {
     url: "https://play.google.com/store/apps/details?id=com.ofcsoftwareDevops.rccgsundayschoolmanual",  // ← update if needed
-    badge: null
+    badge: "images/get-it-on-google-play.svg"
   },
   appStore: {
-    url: "apps.apple.com/app/rccg-sunday-school-manual/id6759505007",
-    badge: null
+    url: "https://apps.apple.com/app/rccg-sunday-school-manual/id6759505007",
+    badge: "images/download-on-app-store.svg"
   }
 };

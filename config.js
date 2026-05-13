14fe763719096ed006a52449af351f72e50f4ffc
OFCsoftwareDevops <olaoluwa.ogunseye@gmail.com>
2026-05-13 21:57:00 +0200

    Update config.js

--- config.js
+++ config.js
@@ -9,3 +9,14 @@ const appLinks = {
     badge: "images/download-on-app-store.svg"
   }
 };
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDSB9-HObJR7NcMJStqE8UnT-4L6J7_tEw",
+  authDomain: "sundayschoolmanual-e2951.firebaseapp.com",
+  projectId: "sundayschoolmanual-e2951",
+  storageBucket: "sundayschoolmanual-e2951.firebasestorage.app",
+  messagingSenderId: "805653977002",
+  appId: "1:805653977002:web:2dd44b21dc33150e15bdd3",
+  measurementId: "G-MEWL8ZD198"
+};

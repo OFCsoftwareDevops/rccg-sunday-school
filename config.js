75d890c1dc9db83e19d8197fc943d3f147bd3ac9
OFCsoftwareDevops <olaoluwa.ogunseye@gmail.com>
2026-05-31 21:20:58 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
-const appLinks = {
+/*const appLinks = {
   playStore: {
     url: "https://play.google.com/store/apps/details?id=com.ofcsoftwareDevops.rccgsundayschoolmanual",  // ← update if needed
     badge: "images/get-it-on-google-play.svg"
@@ -22,3 +22,29 @@ const firebaseConfig = {
 };
 
 export default firebaseConfig;
+*/
+
+// config.js
+
+window.appConfig = {
+  stores: {
+    playStore: {
+      url: "https://play.google.com/store/apps/details?id=com.ofcsoftwareDevops.rccgsundayschoolmanual",
+      badge: "images/get-it-on-google-play.svg"
+    },
+    appStore: {
+      url: "https://apps.apple.com/app/rccg-sunday-school-manual/id6759505007",
+      badge: "images/download-on-app-store.svg"
+    }
+  },
+
+  firebase: {
+    apiKey: "AIzaSyDSB9-HObJR7NcMJStqE8UnT-4L6J7_tEw",
+    authDomain: "sundayschoolmanual-e2951.firebaseapp.com",
+    projectId: "sundayschoolmanual-e2951",
+    storageBucket: "sundayschoolmanual-e2951.firebasestorage.app",
+    messagingSenderId: "805653977002",
+    appId: "1:805653977002:web:2dd44b21dc33150e15bdd3",
+    measurementId: "G-MEWL8ZD198"
+  }
+};
